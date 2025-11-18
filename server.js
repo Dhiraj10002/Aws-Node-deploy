a396ae408ac3c9779e323664513a980f51baa90a
dhiraj10002 <129248943+Dhiraj10002@users.noreply.github.com>
2025-11-18 20:05:47 +0530

    add .env

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config
 
 app.get('/api/get',(req, res) => {
     res.send({message: "hello i am from backend"})
@@ -11,11 +12,11 @@ app.get('/api/get_user_details',(req, res) => {
             name: "Dhiraj",
             age: 24,
             city: "Bangalore"
-        }
+        },env:process.env.NAME
     })
 })
 
 
-app.listen(8000, () =>{
+app.listen(process.env.PORT, () =>{
     console.log("server is running on port 8000");
 })
