8508c8a24fc49ab078ff341e27c1ea1052dfca64
dhiraj10002 <129248943+Dhiraj10002@users.noreply.github.com>
2025-11-18 19:47:59 +0530

    get user details

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get('/api/get',(req, res) => {
     res.send({message: "hello i am from backend"})
 })
 
+app.get('/api/get_user_details',(req, res) => {
+    res.send({
+        user:{
+            name: "Dhiraj",
+            age: 24,
+            city: "Bangalore"
+        }
+    })
+})
+
+
 app.listen(8000, () =>{
     console.log("server is running on port 8000");
 })
